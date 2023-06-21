d5cd24b84c966cfacc4d43352ae6ede98a6a1332
Nathan Hadley <nathan.hadley@me.com>
2023-06-21 13:50:34 -0700

    add production config mail details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'api',
+    :password => '7225f0dd23ae706a2d26292b21c6f434',
+    :address => 'live.smtp.mailtrap.io',
+    :domain => 'live.smtp.mailtrap.io',
+    :port => '587',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
